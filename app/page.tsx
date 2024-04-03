3c386c17aba1f627d5e80ac305902bdcc0028052
Alper Demir <demir.alper@outlook.com.tr>
2024-04-03 21:29:36 +0300

    data fetching with swr added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+"use client";
 import Logo from "@/components/logo";
+import { useState } from "react";
+import useDebounce from "@/hooks/useDebounce";
+import useWeather from "@/hooks/useWeather";
+import Image from "next/image";
 
 export default function Home() {
+  const [search, setSearch] = useState<string>("");
+  const debouncedSearch = useDebounce(search, 500);
+  const { weather, isLoading, isError } = useWeather(debouncedSearch);
+  console.log(weather);
+
   return (
     <div>
       <div className="">
@@ -15,12 +25,32 @@ export default function Home() {
                 Choose a location to see the weather forecast
               </p>
             </div>
-            <div>
+            <div className=" relative">
               <input
                 type="text"
-                className=" w-full rounded-lg px-5 h-14 focus:outline-none bg-[#1E1E29] placeholder:text-base-400 text-base-white text-[16px] leading-[22.4px]"
+                className="  w-full rounded-lg px-5 h-14 focus:outline-none bg-[#1E1E29] placeholder:text-base-400 text-base-white text-[16px] leading-[22.4px]"
                 placeholder="Search location"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
               />
+
+              {isLoading && (
+                <Image
+                  src="/spinner.svg"
+                  width={32}
+                  height={32}
+                  alt="spinner"
+                  className=" absolute top-3 right-3 animate-spin"
+                />
+              )}
+              {isError && <p>Error</p>}
+              {weather && (
+                <div>
+                  <h2>
+                    {weather.name} - {weather.sys.country}
+                  </h2>
+                </div>
+              )}
             </div>
           </div>
         </div>
---
+++ hooks/useWeather.ts
@@ -0,0 +1,17 @@
+import useSWR from "swr";
+import fetcher from "@/libs/fetcher";
+
+const useWeather = (city: string) => {
+  const { data, error, isLoading } = useSWR(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`,
+    fetcher
+  );
+
+  return {
+    weather: data,
+    isLoading,
+    isError: error,
+  };
+};
+
+export default useWeather;
